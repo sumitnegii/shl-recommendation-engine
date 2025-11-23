ab7bb0270b53fe2289086dbb1f9814d8ee17e119
sumitnegii <negisumit308@gmail.com>
2025-11-23 12:05:12 +0530

    Add/fix frontend package.json

---
+++ frontend/.next/dev/static/chunks/8aea3_next_app_cfb2aa32.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
+"[project]/Desktop/projects/shl-recommendation-engine/frontend/node_modules/next/app.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+module.exports = __turbopack_context__.r("[project]/Desktop/projects/shl-recommendation-engine/frontend/node_modules/next/dist/pages/_app.js [client] (ecmascript)");
+}),
+]);
+
+//# sourceMappingURL=8aea3_next_app_cfb2aa32.js.map
---
+++ frontend/.next/dev/static/chunks/Desktop_projects_shl-recommendation-engine_frontend_pages__error_2da965e7._.js
@@ -0,0 +1,15 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/8aea3_next_dist_compiled_8f65a0bc._.js",
+  "static/chunks/8aea3_next_dist_shared_lib_102877fd._.js",
+  "static/chunks/8aea3_next_dist_client_1ea6041b._.js",
+  "static/chunks/8aea3_next_dist_1af3c002._.js",
+  "static/chunks/8aea3_next_error_7f1bd40f.js",
+  "static/chunks/[next]_entry_page-loader_ts_2232fd65._.js",
+  "static/chunks/8aea3_react-dom_3fcd4b76._.js",
+  "static/chunks/8aea3_0c123297._.js",
+  "static/chunks/[root-of-the-server]__acbcfc29._.js"
+],
+    source: "entry"
+});
